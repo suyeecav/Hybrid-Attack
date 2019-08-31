ef7010a86422ce0b03d2c1bee2a00bc92c12754b
Suyee <fs5xz@virginia.edu>
2019-08-30 21:20:48 -0400

    nes_run.sh

--- cifar10/nes_run.sh
+++ cifar10/nes_run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # nes untargeted baseline attack on robust target model 
-<<<<<<< HEAD
 python3 hybrid_attack.py -n 100 --attack_method nes --attack_type untargeted --model_type robust --local_model_names no_local_model --no_save_model
 
 # nes untargeted hybrid attack on robust target model with normal local models
@@ -12,23 +11,10 @@ python3 hybrid_attack.py -n 100 --load_imgs --with_local --no_tune_local --attac
 
 # nes untargeted hybrid attack (with local model tuning) on robust target model with normal local models
 python3 hybrid_attack.py -n 100 --load_imgs --with_local --attack_method nes --attack_type untargeted --model_type robust --local_model_names modelB modelD modelE --no_save_model
-=======
-python3 hybrid_attack.py -n 100 --attack_method nes --attack_type untargeted --model_type robust --local_model_names no_local_model
-
-# nes untargeted hybrid attack on robust target model with normal local models
-python3 hybrid_attack.py -n 100 --load_imgs --with_local --no_tune_local --attack_method nes --attack_type untargeted --model_type robust --local_model_names modelB modelD modelE
-
-# nes untargeted hybrid attack on robust target model with robust local models
-python3 hybrid_attack.py -n 100 --load_imgs --with_local --no_tune_local --attack_method nes --attack_type untargeted --model_type robust --local_model_names adv_densenet adv_resnet
-
-# nes untargeted hybrid attack (with local model tuning) on robust target model with normal local models
-python3 hybrid_attack.py -n 100 --load_imgs --with_local --attack_method nes --attack_type untargeted --model_type robust --local_model_names modelB modelD modelE
->>>>>>> 2978ab4b6e691caffd6c99644da8d57598c322ee
 
 
 
 # nes targeted baseline attack on normal target model 
-<<<<<<< HEAD
 python3 hybrid_attack.py -n 100 --attack_method nes --attack_type targeted --model_type normal --local_model_names no_local_model --no_save_model
 
 # nes targeted hybrid attack on normal target model with normal local models
@@ -39,15 +25,3 @@ python3 hybrid_attack.py -n 100 --load_imgs --with_local --no_tune_local --attac
 
 # nes targeted hybrid attack (with local model tuning) on normal target model with normal local models
 python3 hybrid_attack.py -n 100 --load_imgs --with_local --attack_method nes --attack_type targeted --model_type normal --local_model_names modelB modelD modelE --no_save_model
-=======
-python3 hybrid_attack.py -n 100 --attack_method nes --attack_type targeted --model_type normal --local_model_names no_local_model
-
-# nes targeted hybrid attack on normal target model with normal local models
-python3 hybrid_attack.py -n 100 --load_imgs --with_local --no_tune_local --attack_method nes --attack_type targeted --model_type normal --local_model_names modelB modelD modelE
-
-# nes targeted hybrid attack on normal target model with robust local models
-python3 hybrid_attack.py -n 100 --load_imgs --with_local --no_tune_local --attack_method nes --attack_type targeted --model_type normal --local_model_names adv_densenet adv_resnet
-
-# nes targeted hybrid attack (with local model tuning) on normal target model with normal local models
-python3 hybrid_attack.py -n 100 --load_imgs --with_local --attack_method nes --attack_type targeted --model_type normal --local_model_names modelB modelD modelE
->>>>>>> 2978ab4b6e691caffd6c99644da8d57598c322ee
